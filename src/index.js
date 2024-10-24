96fbfb7d1dc760cb11aa17d37b610c84d5c6aa81
maheshparajuli <maheshparajuli717@gmail.com>
2024-10-24 20:06:29 +0545

    changes done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const myFirstElement = <h1>Hello React!</h1>
+import App from './App';  // Importing the App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(<App />); 
