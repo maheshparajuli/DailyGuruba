b01a91be5d1f654b2ac43139535e27f6db557ffd
maheshparajuli <maheshparajuli717@gmail.com>
2024-10-24 19:17:02 +0545

    edited app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-// import logo from './logo.svg';
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  // Use state to manage the content of the <h1> element
+  const [heading, setHeading] = useState("Let's Start!");
+
+  // Function to update the state
+  const clickme = () => {
+    setHeading("A new beginning");
+  };
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      {/* Use the state variable 'heading' to set the content of <h1> */}
+      <h1 id="newid">{heading}</h1>
+      {/* Corrected the onClick syntax */}
+      <button onClick={clickme}>Click me Lekhnath</button>
     </div>
   );
 }
