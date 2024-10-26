4c2b63325f2fd32108482026a66967c1ad2994ed
maheshparajuli <maheshparajuli717@gmail.com>
2024-10-26 20:43:18 +0545

    changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Home from './Components/Home';
-import About from './Components/About';
-import Projects from './Components/Projects';
-import Contacts from './Components/Contacts';
-import Footer from './Components/Footer';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Contacts from "./Components/Contacts";
+import Footer from "./Components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contacts />} />
-      </Routes>
+      <div className="app-container">
+        {" "}
+        {/* Added app-container here */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contacts />} />
+        </Routes>
+      </div>
       <Footer />
     </Router>
   );
