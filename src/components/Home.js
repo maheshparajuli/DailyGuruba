9b9a2ac011f51bca61df4a69744a2efd2a52b7d6
maheshparajuli <maheshparajuli717@gmail.com>
2024-10-29 22:35:04 +0545

    lets see

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,35 @@
-
-// Home.js
 import React from 'react';
+import photo1 from '../assets/photo1.jpg';
+import photo2 from '../assets/photo2.jpg';
+import photo3 from '../assets/photo3.jpg';
 
-function Home() {
-  return (
-    <section id="home" className="home">
-      <h2>Welcome to My Portfolio</h2>
-      <p>Hello! I'm Mahesh Parajuli, a passionate developer with expertise in ReactJS and computational mathematics.</p>
-    </section>
-  );
-}
+
+
+
+const Home = () => {
+    const photos = [
+        { src: photo1, date: '2023-01-10', caption: 'New Year Celebration' },
+        { src: photo2, date: '2023-02-14', caption: 'Valentine\'s Day Out' },
+        { src: photo3, date: '2023-04-05', caption: 'Spring Blossoms' },
+       
+    ];
+
+    return (
+        <div className="home-container">
+            <h1>Welcome to My Gallery</h1>
+            <p>Here are some of my favorite moments.</p>
+            
+            <div className="photo-gallery">
+                {photos.map((photo, index) => (
+                    <div key={index} className="photo-item">
+                        <img src={photo.src} alt={photo.caption} className="gallery-photo" />
+                        <p className="photo-date">{photo.date}</p>
+                        <p className="photo-caption">{photo.caption}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default Home;
