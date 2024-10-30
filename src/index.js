d4bd52f1c14af5ae35cac485d976884db66f4454
maheshparajuli <maheshparajuli717@gmail.com>
2024-10-30 13:19:39 +0545

    learned new thing

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';  // Importing the App component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />); 
+const container=document.getElementById('root')
+
+const root = ReactDOM.createRoot(container)
+
+root.render(<App />);
