4252c8a2a9ca9d659eb8612633926621a20f897c
maheshparajuli <maheshparajuli717@gmail.com>
2024-10-31 21:47:48 +0545

    done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const isto = document.getElementById("root");
-
 const root = ReactDOM.createRoot(isto);
-
 root.render(<App />);
