366232814dca263fb79d850b6c33208fb3f9e132
maheshparajuli <maheshparajuli717@gmail.com>
2024-11-02 18:14:34 +0545

    some animations on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,17 @@
 // Footer.js
-import React from 'react'; // Optional, if you have separate styles
+import React from 'react';
+
 
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} Mahesh Parajuli. All rights reserved.</p>
+      <div className="footer-content">
+        <p className="copyright">
+          &copy; {new Date().getFullYear()} Mahesh Parajuli. All rights reserved.
+        </p>
+        <div className="footer-decoration"></div>
+      </div>
     </footer>
   );
 }
-
 export default Footer;
